6b0858aff42a154fe2cd52be50638422d1d590f5
Rohit Yadav <rohityadav9641@hotmail.com>
2025-12-19 22:22:49 +0530

    loadBalancer

--- userservice/src/main/java/com/rohit/userservice/controller/UserController.java
+++ userservice/src/main/java/com/rohit/userservice/controller/UserController.java
@@ -1,5 +1,7 @@
 package com.rohit.userservice.controller;
 
+
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,8 +11,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/users/public")
 public class UserController {
 
+    @Value("${server.port}")
+    private String port;
+
     @GetMapping("/all")
     public String getAll(@RequestHeader("X-Auth-User") String username){
-        return "Request handled for user: " + username;
+        return "Request handled for user: " + username + "server: "+port;
     }
 }
